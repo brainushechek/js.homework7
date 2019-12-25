0b86b7808f351d5dcda7299d334f14bb3c9d4920
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-25 17:24:38 +0200

    modificate task 01

--- 01.js
+++ 01.js
@@ -1,6 +1,17 @@
 var arr = [1, 4, 5, [8, 0, 10, 3], 6, 64, 64, 54, {key: 'name', age: 18}]
-arr.forEach( function elementOutput(val) {
-    if (typeof(val) === 'object'){
-        Object.values(val).forEach((val) => elementOutput(val))
-    } else console.log(val)
-})
+function recurs(argument) {
+    var currentValue = argument.shift()
+    if (typeof(currentValue) === 'object' && currentValue !== null) {
+        var valuesOfObject = Object.values(currentValue)
+        recurs(valuesOfObject)
+        valuesOfObject.shift()
+    }
+    if (typeof(currentValue) !== 'object' || currentValue === null) {
+        console.log(currentValue)
+    }
+    if (argument.length) {
+        recurs(argument)
+    }
+}
+
+recurs(arr)
