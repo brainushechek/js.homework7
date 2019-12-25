d57e34ab2e8e86a69319ebdf3798a4713aecad5d
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-25 17:28:28 +0200

    modificate task 05

--- 05.js
+++ 05.js
@@ -1,9 +1,6 @@
 var arr = [1, 10, -2, 12, 3, 5]
-var newArr = new Array()
-arr.forEach(val => {
-	if(val > 0 && val < 10) {
-		newArr.push(val)
+var newArr = arr.filter(val => {
+	return val > 0 && val < 10 
 	}
-}
 )
 console.log(newArr)
