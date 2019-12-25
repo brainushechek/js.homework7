b581a1024f9ce7ceed2909ee49b2c2a2ee43a057
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-25 17:35:16 +0200

    modification task 08

--- 08.js
+++ 08.js
@@ -1,8 +1,5 @@
 var arr = [1, 10, -2, 12, 3, 5]
-var newArr = new Array()
-arr.forEach(val => {
-	if(val % 2 === 0) {
-		newArr.push(val)
-	}
+var newArr = arr.filter(val => {
+	return val % 2 === 0
 })
 console.log(newArr)
